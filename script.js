679f2dd27f2d6e0f98b14d5148cf6f42f93f4bce
emonja <emonja@gmail.com>
2025-10-25 16:38:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-const svg = d3.select("#timeline");
-const margin = { top: 80, right: 40, bottom: 80, left: 40 };
-const width = 1800 - margin.left - margin.right;
+// D3.js Horizontal Timeline Visualization
+
+const margin = { top: 50, right: 40, bottom: 50, left: 40 };
+const width = window.innerWidth - margin.left - margin.right - 100;
 const height = 400 - margin.top - margin.bottom;
 
-const g = svg
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+const svg = d3.select("#timeline")
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const color = d3.scaleOrdinal()
-  .domain(["Visual", "Sound", "Interactive", "Digital"])
-  .range(["#ff8c00", "#1e90ff", "#8a2be2", "#2ecc71"]);
-
-const tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
-
 d3.json("data/media_timeline.json").then(data => {
+  // Parse years
+  data.forEach(d => d.year = +d.year);
+
+  // Create scales
   const x = d3.scaleLinear()
     .domain(d3.extent(data, d => d.year))
     .range([0, width]);
 
-  const xAxis = d3.axisBottom(x)
-    .tickFormat(d => d < 0 ? `${-d} BCE` : d)
-    .ticks(20);
-
-  g.append("g")
-    .attr("transform", `translate(0,${height / 2})`)
+  // Timeline axis
+  const xAxis = d3.axisBottom(x).tickFormat(d3.format("d"));
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
     .call(xAxis);
 
-  const events = g.selectAll(".event")
+  // Event markers
+  svg.selectAll(".event")
     .data(data)
     .enter()
-    .append("g")
+    .append("circle")
     .attr("class", "event")
-    .attr("transform", d => `translate(${x(d.year)},${height / 2})`);
+    .attr("cx", d => x(d.year))
+    .attr("cy", height / 2)
+    .attr("r", 6)
+    .attr("fill", d => {
+      if (d.category === "Visual") return "#2a9d8f";
+      if (d.category === "Sound") return "#e76f51";
+      if (d.category === "Interactive") return "#f4a261";
+      if (d.category === "Digital") return "#264653";
+      return "#999";
+    });
 
-  events.append("circle")
-    .attr("class", "event-circle")
-    .attr("r", 8)
-    .attr("fill", d => color(d.category))
+  // Tooltip
+  const tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
+  svg.selectAll(".event")
     .on("mouseover", (event, d) => {
-      tooltip.transition().duration(200).style("opacity", .9);
+      tooltip.transition().duration(200).style("opacity", 0.95);
       tooltip.html(`<strong>${d.title}</strong><br>${d.year}<br>${d.description}`)
         .style("left", (event.pageX + 10) + "px")
-        .style("top", (event.pageY - 28) + "px");
+        .style("top", (event.pageY - 40) + "px");
     })
     .on("mouseout", () => tooltip.transition().duration(300).style("opacity", 0));
-
-  events.append("text")
-    .attr("class", "event-label")
-    .attr("y", (d, i) => (i % 2 === 0 ? -20 : 30))
-    .text(d => d.title);
 });
